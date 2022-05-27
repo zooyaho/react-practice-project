fa8137b8a9426416becf86b779024246cf69f269
zooyaho <jiwoo1268@naver.com>
2022-05-27 21:28:34 +0900

    ExpenseForm컴포넌트 생성, NewExpense 스타일링 완료

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,1 +1,14 @@
 /* 비용 데이터 입력 컴포넌트 */ 
+import React from 'react'; // jsx가 사용하는 react라이브러리
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = ()=>{
+  return (
+    <div className='new-expense'>
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;

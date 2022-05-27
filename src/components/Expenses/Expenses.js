fb390c3d4d194d7a8783a406757b5eb442200008
zooyaho <jiwoo1268@naver.com>
2022-05-28 00:27:55 +0900

    ExpenseFilter.js컴포넌트에서 선택한 dropdown Year값을 Expenses.js컴포넌트로 전달

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,20 @@
+import React, {useState} from 'react';
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css"
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props){
+  const [filteredYear, setFilteredYear] = useState('2021');
+
+  const filterChangeHandler = (selectedYear)=>{
+    setFilteredYear(selectedYear);
+    console.log('In Expenses.js', selectedYear);
+  }
 
   return(
     <Card className="expenses">
+      <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}

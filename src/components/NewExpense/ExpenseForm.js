948da7f37f6d8c3fb5062f935a2e2936da269cdf
zooyaho <jiwoo1268@naver.com>
2022-05-27 23:12:54 +0900

    submit으로 전송 시 폼 요소들의 value 초기화

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -31,8 +31,12 @@ const ExpenseForm = ()=>{
       amount: enteredAmount,
       date: new Date(enteredDate)
     };
-
     console.log(expenseData);
+
+    // 폼요소 초기화
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -40,15 +44,15 @@ const ExpenseForm = ()=>{
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+          <input type='number' value={enteredAmount} min='0.01' step='0.01' onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2020-01-01' max='2023-12-31' onChange={dateChangeHandler}/>
+          <input type='date' value={enteredDate} min='2020-01-01' max='2023-12-31' onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className='new-expense__actions'>

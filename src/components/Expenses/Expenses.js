d50dc30a9a27618a69d516fd8c747afdd2c4e1d6
zooyaho <jiwoo1268@naver.com>
2022-05-28 13:15:26 +0900

    App.js에 있는 expenses 배열 개수에 맞게 <ExpenseItem />컴포넌트 동적 렌더링되게 수정

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,42 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css"
+import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
-function Expenses(props){
-  const [filteredYear, setFilteredYear] = useState('2021');
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2021");
 
-  const filterChangeHandler = (selectedYear)=>{
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log('In Expenses.js', selectedYear);
-  }
+    console.log("In Expenses.js", selectedYear);
+  };
 
-  return(
+  return (
     <Card className="expenses">
-      <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+      <ExpensesFilter
+        onChangeFilter={filterChangeHandler}
+        selected={filteredYear}
       />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 }
 
-export default Expenses
+export default Expenses;

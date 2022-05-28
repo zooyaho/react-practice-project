0ae73d1e375aa287bd2f3a4e2546848096e41766
zooyaho <jiwoo1268@naver.com>
2022-05-28 19:57:48 +0900

    Add New Expense버튼, Cancel버튼을 생성해 폼의 생성 여부 결정하는 기능 추가

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,25 +1,43 @@
-/* 비용 데이터 입력 컴포넌트 */ 
-import React from 'react'; // jsx가 사용하는 react라이브러리
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+/* 비용 데이터 입력 컴포넌트 */
+import React, { useState } from "react"; // jsx가 사용하는 react라이브러리
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
-const NewExpense = (props)=>{
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
 
   // 자식에서 부모 컴포넌트로 소통하는 방법
-  const saveExpenseDataHamdler = (enteredExpenseData)=>{
+  const saveExpenseDataHandler = (enteredExpenseData) => {
     // ExpenseForm에서 추가한 요소의 값들을 저장함.
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
 
     // App.js컴포넌트로 onAddExpense함수 실행하여, expenseData 보냄.
     props.onAddExpense(expenseData);
+
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHamdler} />
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };

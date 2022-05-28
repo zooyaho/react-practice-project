eba615b20501e204d28cf6a76954b3ed869134fd
zooyaho <jiwoo1268@naver.com>
2022-05-28 14:59:27 +0900

    ExpensesList 컴포넌트 생성해 필터링 된 espenses list 출력

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2021");
@@ -17,20 +17,7 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p> No expenses found. </p>;
-
-  if(filteredExpenses.length > 0){
-    expensesContent = filteredExpenses.map((expense) => (
-      /* key={}: 추가하는 item배열의 순서를 react에게 알려야함. 그렇지 않으면 새로운 item을 맨 뒤에 추가 후 우리가 정한 배열에 맞게 다시 렌더링함-> 성능 측면에서 좋지 않게 됨. */
-      /* html요소나 컴포넌트에 'key추가 시' 리액트가 개별 아이템을 인식할 수 있도록 도와줌. => 결론은 목록의 아이템을 매핑할때 항상 key를 추가해야함!!!*/
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
+  
 
   return (
     <Card className="expenses">
@@ -38,7 +25,7 @@ function Expenses(props) {
         onChangeFilter={filterChangeHandler}
         selected={filteredYear}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses}/>
     </Card>
   );
 }
